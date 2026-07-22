0bc775c35234de277ecaa1812723fc39b371be4f
Mosaic93-cn <Mosaic93-cn@users.noreply.github.com>
2026-07-22 03:42:16 +0000

    🚀 Deploy: fix: 支持悬停后续节点预览路线 Mosaic93-cn/-terminal-courtyard@663f2b786e4494045f0205b91cecd1a279c7125d

--- assets/BufferResource-D7wxaiC9.js
+++ assets/BufferResource-D7wxaiC9.js
@@ -1,4 +1,4 @@
-import{w as U,r as g,s as c,u as S,B as _,t as m,E as I,x as p}from"./index-B26G5mDz.js";const x={name:"local-uniform-bit",vertex:{header:`
+import{w as U,r as g,s as c,u as S,B as _,t as m,E as I,x as p}from"./index-zM1RHATn.js";const x={name:"local-uniform-bit",vertex:{header:`
 
             struct LocalUniforms {
                 uTransformMatrix:mat3x3<f32>,
